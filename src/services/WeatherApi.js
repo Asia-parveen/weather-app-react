ae1f9568eb7f9d294b9fba0d4bf6d11c2d9e5bc2
Asia-parveen <beautyhealthytips177@gmail.com>
2025-01-19 13:02:40 -0800

    added

--- src/services/WeatherApi.js
+++ src/services/WeatherApi.js
@@ -1,17 +1,6 @@
 import axios from "axios";
 
-// const apiKey = "c092409e7122a0be3ac767d120eae5cd"; // Replace with your API key
 
-// export const fetchWeatherData = async (city) => {
-//   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-//   try {
-//     const response = await axios.get(url);
-//     return response.data;
-//   } catch (error) {
-//     throw new Error("Failed to fetch weather data");
-//   }
-// };
-// weatherAPI.js
 export const fetchWeatherData = async (city) => {
     const apiKey = "c092409e7122a0be3ac767d120eae5cd";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
